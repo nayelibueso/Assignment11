dcaf134fab714cbd44097203f418ac6c2914dbfd
nayelibueso <nayelibueso50@gmail.com>
2024-09-02 01:14:27 -0500

    Trying to get Transaction Detail page working

--- src/main/java/com/codercampus/Assignment11/service/TransactionService.java
+++ src/main/java/com/codercampus/Assignment11/service/TransactionService.java
@@ -1,6 +1,7 @@
 package com.codercampus.Assignment11.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,15 +12,25 @@ import com.codercampus.Assignment11.repository.TransactionRepository;
 @Service
 public class TransactionService {
 
+	
+	private final TransactionRepository transactionRepo;
+	
 	@Autowired
-	TransactionRepository transactionRepo;
-
 	public TransactionService(TransactionRepository transactionRepo) { // Constructor for dependency issues
 		this.transactionRepo = transactionRepo;
 	}
 
 	public List<Transaction> findAll() {
-		return transactionRepo.findAll();
+		return transactionRepo.findAll().stream()
+							  .sorted((t1, t2)-> t1.getDate().compareTo(t2.getDate())) //Sorting by date in ascending order
+							  .collect(Collectors.toList());
+	}
+
+	public Transaction findById(Long transactionId) {
+		return transactionRepo.findAll().stream()
+					           .filter(transaction -> transaction.getId().equals(transactionId))
+					           .findFirst()
+					           .orElseThrow(() -> new RuntimeException("Transaction not found"));
 	}
 
 }
--- src/main/java/com/codercampus/Assignment11/web/TransactionalController.java
+++ src/main/java/com/codercampus/Assignment11/web/TransactionalController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.codercampus.Assignment11.domain.Transaction;
 import com.codercampus.Assignment11.service.TransactionService;
 
 @Controller
@@ -20,6 +23,13 @@ public class TransactionalController {
 	@GetMapping("/transactions")
 	public String getAllTransactions(Model model) { // Responsible for fetching all transactions and them to the view.
 		model.addAttribute("transactions", transactionService.findAll());
+		return "transactions";
+	}
+	
+	@GetMapping("/transactions/{transactionId}")
+	public String getTransactionDetail (@PathVariable Long transactionId, Model model) {
+		Transaction selectedTransaction = transactionService.findById(transactionId);
+		model.addAttribute("selectedTransaction", selectedTransaction);
 		return "transactions";
 	}
 
